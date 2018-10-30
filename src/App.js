1b0ebfdf767c24a507eee2a11e8944dc1c20e4ab
Bharat Raj Meriyala <bharatrm@hotmail.com>
2018-10-30 16:00:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React For the first time
+            Learn React For the first time onto a feature branch
           </a>
         </header>
       </div>
